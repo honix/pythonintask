3a9efd2f220f27efabcdbf84620b1750dda693ec
honix <ted888@ya.ru>
2016-03-09 00:12:56 +0300

    Задача 5

---
+++ IVTa/2014/SHCHUKIN_F_O/task_5_30.py
@@ -0,0 +1,31 @@
+﻿# Задача 5. Вариант 30.
+'''
+Напишите программу, которая бы при запуске случайным образом
+отображала имя одного из трех племянников Скруджа МакДака.
+'''
+# Shchukin F.O.
+# 08.03.2016
+
+from random import choice
+
+phrase = 'Билли, Вилли и Дилли'
+
+nephews = [''.join(filter(str.isalpha, name))
+           for name in phrase.split(' ')
+           if len(name) > 2]
+
+stats = [0, 0, 0]
+
+k = 1
+while True:
+    c = choice(nephews)
+    stats[nephews.index(c)] += 1
+    k -= 1
+    if k <= 0:
+        print('\n' + c)
+        print('Статистика ' + str(stats))
+        try:
+            k = int(input('Повторить n раз: '))
+        except:
+            continue
+
