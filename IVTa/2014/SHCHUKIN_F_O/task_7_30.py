f05baef3e2f8a8cd78b3e905aa4ad28324ad3012
honix <ted888@ya.ru>
2016-03-22 21:59:20 +0300

    Задача 7

---
+++ IVTa/2014/SHCHUKIN_F_O/task_7_30.py
@@ -0,0 +1,53 @@
+# Задача 6. Вариант 30.
+'''
+Создайте игру, в которой компьютер загадывает название
+одного из двенадцати месяцев, а игрок должен его угадать.
+'''
+# Shchukin F.O.
+# 20.03.2016
+
+from random import choice
+
+print('Угадайте один из 12 месяцев!')
+
+months = tuple('Январь Февраль Март Апрель Май Июнь Июль\
+         Август Сентябрь Октябрь Ноябрь Декабрь'.split())
+
+months = [x.lower() for x in months]
+
+right = choice(months)
+trys = []
+
+score_unit = 5
+score = score_unit * len(months)
+
+k = 1
+while True:
+    print('\n', k, 'попытка. У вас', score, 'очков.')
+    word = input('> ').lower()
+    
+    if right == word:
+        print('Правильно! Это', right + '!')
+        print('Вы заработали', score, 'очков!')
+        input('\nНамите Enter...')
+        break
+        
+    if word not in months:
+        print('Такого месяца нет')
+        continue
+    
+    if word in trys:
+        print('Вы уже пробовали', word)
+        continue
+    trys.append(word)
+    score -= score_unit
+    
+    k += 1
+    if k == 4:
+        print('Подсказка: последниие две буквы -', right[-2:])
+        
+    elif k == 6:
+        r = months.index(right)
+        print('Подсказка:', months[r-1], '...',
+              months[r+1 if r < len(months)-1 else 0])
+        
